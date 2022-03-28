b863cab559dc41f8e52402942f1b3206d8232f4f
vignashwar <vignashwarv@gmail.com>
2022-03-29 00:13:15 +1100

    create top nav

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>soru4all</title>
+    <link rel="stylesheet" href="/assets/css/main.css">
+    
+</head>
+<body>
+    <header>
+        <nav class="top-nav">
+            <div class="container">
+                <div class="top-nav-content">
+                    <div class="phone-wrapper">
+                        <a class="phone" href="">
+                            <img src="/assets/img/phone-solid.svg" alt="">
+                            <span>1300 881 331</span>
+                        </a>
+                    </div>
+                    <ul>
+                        <li><img src="/assets/img/facebook-brands.svg" alt=""></li>
+                        <li><img src="/assets/img/twitter-brands.svg" alt=""></li>
+                        <li><img src="/assets/img/youtube-brands.svg" alt=""></li>
+                    </ul>
+                </div>
+
+            </div>
+        </nav>
+
+    </header>
+    
+    <script src="/assets/js/main.js"></script>
+</body>
+</html>
